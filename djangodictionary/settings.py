51285345e5a44302c64be6440c6b4d05855decc1
Nurbek <xamrayevnurbek0@gmail.com>
2022-05-25 18:52:09 +0500

    wqd

--- djangodictionary/settings.py
+++ djangodictionary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1gjq)#c+eel&^2-!im4sv+nf#twjz(9@ig3_e^vm%ygc-+@+-z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
