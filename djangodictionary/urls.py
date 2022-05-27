33c8df7a300410a97438235cdf411418d738de01
Nurbek <xamrayevnurbek0@gmail.com>
2022-05-27 10:53:00 +0500

    wqd

--- djangodictionary/urls.py
+++ djangodictionary/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#sss
+                  path('admin/', admin.site.urls),
+                  path('', include('main.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
+                                                                                         document_root=settings.STATIC_ROOT)
+# sss
--- main/urls.py
+++ main/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path, include
 from main.views import *
 
@@ -22,4 +23,5 @@ urlpatterns = [
 
     path('', HomePageView.as_view(),  name="home"),
     path("search/", SearchResultsView.as_view(), name="search_results"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
+                                                                                         document_root=settings.STATIC_ROOT)
